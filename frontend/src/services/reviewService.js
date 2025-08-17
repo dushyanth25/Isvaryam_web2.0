384de477555c7ebcfba6fc37f58994b9467b937c
dushyanth25 <138096018+dushyanth25@users.noreply.github.com>
2025-08-17 10:56:16 +0530

    Delete frontend directory

--- frontend/src/services/reviewService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getReviewsByProductId = async (productId) => {
-  const { data } = await axios.get(`/api/reviews/product/${productId}`);
-  return data;
-};
